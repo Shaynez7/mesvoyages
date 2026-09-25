8e0b474a9e691e1e4edd941f0447821b2cd7e6a0
Shaynez Zitouni <Shaynez899@outlook.fr>
2026-09-25 17:06:19 +0200

    Phase 10 : ORM (affichage détaillé d'une visite)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -52,4 +52,13 @@ class VoyagesController extends AbstractController{
         ]);
     }
     
+        #[Route('/voyages/voyage/{id}', name: 'voyages.showone')]
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        
+        return $this->render("pages/voyage.html.twig", [
+            'visite' => $visite
+        ]);
+    }
+    
 }
